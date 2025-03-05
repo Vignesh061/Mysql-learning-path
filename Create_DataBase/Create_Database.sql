1877c1687ab2c09675c187837649dd153e71d96a
Vignesh061 <vigneshvicky66005@gmail.com>
2025-03-05 23:21:48 +0530

    Created DataBase

---
+++ Create_DataBase/Create_Database.sql
@@ -0,0 +1,10 @@
+
+-- Syntax For Create a Database 
+
+ 
+Create Database myDb; -- is used to create a database 
+Use myDb; -- use myDb is used to use the DataBase 
+
+
+Drop Database myDb; -- Drop Database is used to Delete a database
+																																							
