472dde08234e418f54d43e02db3cd13f9f196b32
Vignesh061 <vigneshvicky3611@gmail.com>
2025-04-06 21:57:50 +0530

    Learned Wild Card

---
+++ 18.Wild_Card/wild_card.sql
@@ -0,0 +1,18 @@
+
+SELECT * FROM employees
+WHERE first_name LIKE "s%";
+
+SELECT * FROM employees
+WHERE last_name LIKE "%r";
+
+SELECT * FROM employees
+WHERE hire_date LIKE "2023%";
+
+SELECT * FROM employees
+WHERE job LIKE "_ook";
+
+SELECT * FROM employees
+WHERE hire_date LIKE "____-01-__";
+
+SELECT * FROM employees
+WHERE job LIKE "_a%";
